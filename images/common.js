5ef14349f31e60855962a3962c7a5d9a61434cb3
-global <shinyj1991@gmail.com>
2024-07-31 16:06:26 +0900

    feat : 극한의 미니멀리즘 on

--- images/common.js
+++ images/common.js
@@ -3,9 +3,19 @@ const UI = {
     $("body").addClass("scroll-lock");
     $("#drawer").addClass("on");
   },
-  closeDrawer: function (e) {
+  closeDrawer: function () {
     $("body").removeClass("scroll-lock");
     $("#drawer").removeClass("on");
+  },
+  openSearch: function () {
+    $("body").addClass("scroll-lock");
+    $("#search").addClass("on");
+    $('#search input[type="search"]').val("");
+    $('#search input[type="search"]').focus();
+  },
+  closeSearch: function () {
+    $("body").removeClass("scroll-lock");
+    $("#search").removeClass("on");
   },
 };
 
@@ -13,6 +23,11 @@ $(document).ready(() => {
   $("#drawer").on("click", function (e) {
     if (e.currentTarget === e.target) {
       UI.closeDrawer();
+    }
+  });
+  $("#search").on("click", function (e) {
+    if (e.currentTarget === e.target) {
+      UI.closeSearch();
     }
   });
 });
